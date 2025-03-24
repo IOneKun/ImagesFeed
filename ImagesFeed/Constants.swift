f2b8b44699aed3c573a8c033acd937afb6f56ea4
IvanKuninets <ivankuninec8@gmail.com>
2025-03-24 09:48:02 -0300

    Constants

--- ImagesFeed/Constants.swift
+++ ImagesFeed/Constants.swift
@@ -1,9 +1,1 @@
-import Foundation
 
-enum Constants {
-    static let accessKey = "kfwOZigp1im3p-5WHHpSKkknHgUWkjcsG5TdmHe_Y-A"
-    static let secretKey = "eE7qj60gkJlXU4sghpq4xRqzAJ5-HUzMWFQSHzqwKzk"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
-}
